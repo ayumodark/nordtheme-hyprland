10bdbf9682aab75d2fe889ee2d32421daa0cebc0
ayumodark <aayanmohammad@proton.me>
2024-11-04 13:02:29 +0600

    Create treesitter.lua

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-treesitter/nvim-treesitter,
+  config = function()
+    vim.cmd[[TSUpdate]]
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = {lua, bash, python},
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+}
