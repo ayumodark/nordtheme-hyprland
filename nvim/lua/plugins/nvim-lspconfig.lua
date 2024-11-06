26be407af2008f671286c0b2542b7cecd1c7efc0
ayumodark <aayanmohammad@proton.me>
2024-11-06 12:03:52 +0600

    Create nvim-lspconfig.lua

---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  'neovim/nvim-lspconfig'
+  config = function()
+    vim.lsp.set_log_level("debug")
+    require'lspconfig'.pyright.setup{}
+    require'lspconfig'.lua_ls.setup {}
+    require'lspconfig'.bashls.setup{}
+  end,
+}
