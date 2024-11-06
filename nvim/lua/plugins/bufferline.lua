01e8929221f268697fb674451802953c7c163d2f
ayumodark <aayanmohammad@proton.me>
2024-11-06 20:49:20 +0600

    Update bufferline.lua

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,9 @@
 return {
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    vim.opt.termguicolors = true,
+    vim.opt.termguicolors = true
     require("bufferline").setup{}
   end,
 }
