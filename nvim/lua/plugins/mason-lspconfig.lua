4bfb681f8b90b5c1a005cfb8a0a04cfc2a0a719b
ayumodark <aayanmohammad@proton.me>
2024-11-06 12:34:13 +0600

    Update mason-lspconfig.lua

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,10 @@
 return {
-  'williamboman/mason-lspconfig',
-  config = funtion()
-    require('mason-lspconfig').setup(
-      ensure_installed = { 'lua_ls', 'bashls', 'pyright' },
-    )
-  end,
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+        ensure_installed = { "lua_ls", "bashls", "pyright" },
+        require("mason-lspconfig").setup(),
+        require("lspconfig").lua_ls.setup {},
+        require("lspconfig").bashls.setup {},
+        require("lspconfig").pyright.setup {}
+    end,
 }
