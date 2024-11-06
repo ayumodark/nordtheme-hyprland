2b923abd94c790a648a817ecb5b82f8e0904c0fa
ayumodark <aayanmohammad@proton.me>
2024-11-06 12:40:46 +0600

    Update neo-tree.lua

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,18 +1,19 @@
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
-      '3rd/image.nvim',
-    }
-  config = function()
-    require("neo-tree").setup {
-      close_if_last_window = true,
-    }
-    vim.keymap.set("n", "<leader>fr", ":Neotree filesystem reveal right"),
-    vim.keymap.set("n", "<leader>fc", ":Neotree filesystem close right"),
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffer reveal float"),
-  end,
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+        require("neo-tree").setup({
+        close_if_last_window = true,
+        })
+        vim.keymap.set("n", "<leader>fr", ":Neotree filesystem reveal right<CR>", {})
+        vim.keymap.set("n", "<leader>fc", ":Neotree filesystem close right<CR>", {})
+	vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+        vim.keymap.set("n", "<leader>bc", ":Neotree buffers close float<CR>", {})
+    end,
 }
