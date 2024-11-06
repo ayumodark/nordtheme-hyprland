8687fbdaa85eb5a00cfdd7786c38ddb2eba991c8
ayumodark <aayanmohammad@proton.me>
2024-11-06 20:48:27 +0600

    Update alpha.lua

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,26 +1,26 @@
 return {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-	 local dashboard = require'alpha.themes.dashboard'
-        dashboard.section.header.val = {
-	[[                                                                       ]],
-	[[                                                                     ]],
-	[[       ████ ██████           █████      ██                     ]],
-	[[      ███████████             █████                             ]],
-	[[      █████████ ███████████████████ ███   ███████████   ]],
-	[[     █████████  ███    █████████████ █████ ██████████████   ]],
-	[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-	[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-	[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-	[[                                                                       ]],
+	"goolord/alpha-nvim",
+	config = function ()
+		require"alpha".setup(require"alpha.themes.dashboard".config)
+	 local dashboard = require"alpha.themes.dashboard"
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
 	}
-        
-         dashboard.section.buttons.val = {
-             dashboard.button( " ", ">>Lazy" , ":Lazy<CR>"),
-             dashboard.button( " ", ">>Mason" , ":Mason<CR>"),
-             dashboard.button( " ", ">>Neotree", ":Neotree reveal filesystem float<CR>"),
-             dashboard.button( " ", ">>Quit All", ":q!<CR>"),
-         }
-    end
-};
+		
+		dashboard.section.buttons.val = {
+			dashboard.button( " ", "          >> Lazy" , ":Lazy<CR>"),
+			dashboard.button( " ", "          >> Mason" , ":Mason<CR>"),
+      dashboard.button( " ", "          >> Neotree", ":Neotree reveal filesystem float<CR>"),
+      dashboard.button( " ", "          >>Quit All", ":q!<CR>"),
+		}
+	end,
+}
