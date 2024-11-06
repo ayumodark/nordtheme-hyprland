00065f6962d9e469caa9a9cc08ca84b876eb220e
ayumodark <aayanmohammad@proton.me>
2024-11-06 12:30:29 +0600

    Update mason.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,13 +1,14 @@
 return {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup(
-        ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      )
+        require("mason").setup({
+            ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+           }
+        })
     end,
 }
