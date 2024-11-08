5ca2b442a40275af4ca43c12b219a848f0df451e
Ayumo_Dark <aayanmohammad@proton.me>
2024-11-08 17:36:36 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 rm -rf nordtheme-hyprland/.gitignore
 rm -rf nordtheme-hyprland/README.md
 cp -rifa nordtheme-hyperland/* ~/.config
+echo "eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh.omp.json)"" >> ~/.bashrc
+exec bash
 rm -rf nordtheme-hyprland
